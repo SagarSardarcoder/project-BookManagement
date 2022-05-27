907effb89b788c688ed09ee8541f9170e83c1f19
SagarSardarcoder <102857635+SagarSardarcoder@users.noreply.github.com>
2022-05-27 12:32:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,7 +20,14 @@ app.use( multer().any())
 
 app.use('/', route);
 
-
+app.use((req, res, next) => {
+    res.status(404).send({
+        status: 404,
+        error: `Not found ${req.url}`
+        
+    })
+    next()
+})
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
